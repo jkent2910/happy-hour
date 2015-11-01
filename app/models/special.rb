c4bf85d85f3d8dc96e28055f4c3f17c555075185
Julie Kent <jkent2910@gmail.com>
2015-10-31 20:34:08 -0500

    made category and day of the week drop downs

--- app/models/special.rb
+++ app/models/special.rb
@@ -3,4 +3,8 @@ class Special < ActiveRecord::Base
   belongs_to :business 
   validates :name, :category, :price, :day_of_the_week, presence: true
 
+  VALID_DAYS_OF_THE_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  VALID_CATEGORIES = ['Food', 'Drinks - Beer', 'Drinks - Liquor']
+
 end
