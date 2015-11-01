b3fb2826cce717865bb44d4ba25b2d5c6f10f987
Julie Kent <jkent2910@gmail.com>
2015-10-31 21:17:08 -0500

    installed Cocoon and added link to add and remove association for specials

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,7 +3,7 @@ class Business < ActiveRecord::Base
   accepts_nested_attributes_for :address
 
   has_many :specials
-  accepts_nested_attributes_for :specials
+  accepts_nested_attributes_for :specials, reject_if: :all_blank, allow_destroy: true 
 
   validates :name, :start_time, :end_time, presence: true
   validates :name, uniqueness: true 
