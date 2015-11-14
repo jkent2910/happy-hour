d98fa7a7c34ee30d6b51a7a6a117ed671a8da3ed
Julie Kent <jkent2910@gmail.com>
2015-11-14 16:38:49 -0600

    added valid cities

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
 
   validates :line1, :city, :state, :zip, presence: true 
+
+  VALID_CITIES = ['Cedar Rapids', 'Clive', 'Des Moines', 'Iowa City', 'Marion', 'Norwalk', 'Urbandale', 'West Des Moines']
+
 end
