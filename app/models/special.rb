bcfb6fdb93d07ca6c8ad09cabbd35d4dd9631e62
Julie Kent <jkent2910@gmail.com>
2015-11-15 16:55:04 -0600

    formatted and made days of the week an array

--- app/models/special.rb
+++ app/models/special.rb
@@ -3,6 +3,8 @@ class Special < ActiveRecord::Base
   belongs_to :business 
   validates :name, :category, :price, :day_of_the_week, presence: true
 
+  serialize :day_of_the_week, Array
+
   VALID_DAYS_OF_THE_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
   VALID_CATEGORIES = ['Food', 'Drinks - Beer', 'Drinks - Liquor']
